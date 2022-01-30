ea182869ce108709a86b93963c56182b1fac541b
FrankLobo <frankeloboq@gmail.com>
2022-01-30 00:34:46 +0000

    segundo cambio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let generateRandomSuitCard = () => {
+    let cardSuits = ["diams", "hearts", "clubs", "spades"];
+    let indexCardSuits = Math.floor(Math.random() * cardSuits.length);
+    return cardSuits[indexCardSuits];
+  };
+  let generateRandomNumberCard = () => {
+    let cardNumbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexCardNumbers = Math.floor(Math.random() * cardNumbers.length);
+    return cardNumbers[indexCardNumbers];
+  };
+  document.querySelector(
+    ".card"
+  ).className = `card ${generateRandomSuitCard()}`;
+  document.querySelector(".card").innerHTML = generateRandomNumberCard();
 };
+let ButtonCard = document.querySelector(".button-card");
+ButtonCard.addEventListener("click", () => {
+  window.onload();
+});
