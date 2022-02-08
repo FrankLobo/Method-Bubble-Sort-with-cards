ae92f23652374bdd8382838ca8a0915cb2a01302
FrankLobo <frankeloboq@gmail.com>
2022-02-08 02:06:21 +0000

    Finishing Bubble Sort

--- src/app.js
+++ src/app.js
@@ -1,33 +1,44 @@
 window.onload = () => {
+  // let generateRandomSuitCard = () => {
+  //   let cardSuits = ["diams", "hearts", "clubs", "spades"];
+  //   let indexCardSuits = Math.floor(Math.random() * cardSuits.length);
+  //   return cardSuits[indexCardSuits];
+  // };
+  // let generateRandomNumberCard = () => {
+  //   let cardNumbers = [
+  //     "A",
+  //     "2",
+  //     "3",
+  //     "4",
+  //     "5",
+  //     "6",
+  //     "7",
+  //     "8",
+  //     "9",
+  //     "10",
+  //     "J",
+  //     "Q",
+  //     "K"
+  //   ];
+  //   let indexCardNumbers = Math.floor(Math.random() * cardNumbers.length);
+  //   return cardNumbers[indexCardNumbers];
+  // };
   let suits = ["diams", "hearts", "clubs", "spades"];
-  let ranks = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+  let ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
   let objOfCards = [];
+  //Boton que genera cartas aleatorias
   let buttonDraw = document.querySelector(".draw-button");
   buttonDraw.addEventListener("click", myFunctionCards);
+
   function myFunctionCards() {
     objOfCards = [];
     let myCard = document.querySelector(".container");
     if (myCard.childNodes.length !== 0) {
-      // myCard.removeChild("div");
       myCard.innerHTML = "";
     }
     let inputValueCards = parseInt(
       document.getElementById("numberusercards").value
-    ); //estoy llamando al valor del input que coloca el usuario
+    ); //estoy llamando y convirtiendo el valor del input que coloca el usuario en numero
     if (inputValueCards === 0) {
       alert("Coloca un numero mayor a 0");
     } else if (inputValueCards !== 0) {
@@ -39,9 +50,21 @@ window.onload = () => {
         // objOfCards = objOfCards.concat(objElementsCard);
         objOfCards.push(objElementsCard);
       }
-      // console.log(myCard.childNodes.length);
+
       if (myCard.childNodes.length === 0) {
         for (let element of objOfCards) {
+          if (element.ranks === 1) {
+            element.ranks = "A";
+          }
+          if (element.ranks === 11) {
+            element.ranks = "J";
+          }
+          if (element.ranks === 12) {
+            element.ranks = "Q";
+          }
+          if (element.ranks === 13) {
+            element.ranks = "K";
+          }
           let myDiv = document.createElement("div");
           let contentDiv = document.createTextNode(`${element.ranks}`);
           myDiv.className = `card ${element.suits}`;
@@ -51,6 +74,8 @@ window.onload = () => {
       }
     }
   }
+
+  //Algoritmo de ordenamiento de Burbuja
   const bubbleSort = () => {
     let myUl = document.querySelector(".lista-containers-cards-sort");
     if (myUl.childNodes.length !== 0) {
@@ -60,7 +85,20 @@ window.onload = () => {
     while (wall > 0) {
       let index = 0;
       while (index < wall) {
-        console.log(typeof objOfCards[index].ranks);
+        if (objOfCards[index].ranks === "A") {
+          objOfCards[index].ranks = 1;
+        }
+        if (objOfCards[index].ranks === "J") {
+          objOfCards[index].ranks = 11;
+        }
+        if (objOfCards[index].ranks === "Q") {
+          objOfCards[index].ranks = 12;
+        }
+        if (objOfCards[index].ranks === "K") {
+          objOfCards[index].ranks = 13;
+        }
+
+        // console.log(objOfCards[index].ranks);
         //compare the adjacent positions, if the right one is bigger, we have to swap
         if (objOfCards[index].ranks > objOfCards[index + 1].ranks) {
           let aux = objOfCards[index].ranks;
@@ -71,18 +109,34 @@ window.onload = () => {
       }
       wall--; //decrease the wall for optimization
     }
-    console.log(objOfCards);
-    // return objOfCards;
 
     for (let element of objOfCards) {
+      if (element.ranks === 1) {
+        element.ranks = "A";
+      }
+      if (element.ranks === 11) {
+        element.ranks = "J";
+      }
+      if (element.ranks === 12) {
+        element.ranks = "Q";
+      }
+      if (element.ranks === 13) {
+        element.ranks = "K";
+      }
       let myLi = document.createElement("li");
       let contentLi = document.createTextNode(`${element.ranks}`);
       myLi.className = `card ${element.suits}`;
       myLi.appendChild(contentLi);
       myUl.appendChild(myLi);
     }
+    return objOfCards;
   };
 
+  //Boton de Ordenamiento
   let sortButton = document.querySelector(".sort-button");
   sortButton.addEventListener("click", bubbleSort);
 };
+
+// let numbers = [1, 2, 3];
+// let numbersCopy = [numbers];
+// console.log(numbersCopy);
